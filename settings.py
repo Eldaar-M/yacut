8808a0fc333933699d02c4844b8d8d578ef507cc
Eldar Magomedov <emagomedow@yandex.ru>
2024-04-03 11:04:35 +0300

    yacut v 2.0.1

--- settings.py
+++ settings.py
@@ -1,12 +1,13 @@
 import os
 import string
 
-GENERATION_NUMBER = 6
+SHORT_SIZE = 6
 ALLOWED_SYMBOLS = string.ascii_letters + string.digits
-URL_SYMBOLS_REGEXP = f'^[{ALLOWED_SYMBOLS}]*$'
+SHORT_SYMBOLS_REGEXP = f'^[{ALLOWED_SYMBOLS}]*$'
 MAX_SHORT_SIZE = 16
 MAX_ORIGINAL_SIZE = 2048
 REDIRECT_FUNCTION_NAME = 'redirect_view'
+NUMBER_OF_REPETITIONS = 6
 
 
 class Config:
